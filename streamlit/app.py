02013e38415434e47c48eef39bae6a08bc834e71
Mischa Dhar <mischadhar94@gmail.com>
2023-03-21 17:25:24 +0000

    created ultra basic streamlit front end

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,11 +4,16 @@ import plotly.express as px
 from interface import main_pipeline
 from visualizing.nearby_bikes import bikepoints_near_me
 
+#################
+# 'try, except' to prevent error dialogue?
+# button to get user coords based on gps?
+#################
+
 # Title for the homepage
 st.header('Santander bikes near me')
 
 # Load the data function
-@st.cache_data
+@st.cache
 def get_clean_data():
 
     df = main_pipeline()
@@ -24,4 +29,5 @@ bike_loc = st.text_input(label="Please enter the area you want to see:")
 fig = bikepoints_near_me(query=bike_loc
                          , dataframe=df)
 
-st.plotly_chart(fig)
+if bike_loc != "":
+    st.plotly_chart(fig)
