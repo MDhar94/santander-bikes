f656accfcaddea1acd6c6456adcbc44c10b70024
Mischa Dhar <mischadhar94@gmail.com>
2023-03-21 15:26:56 +0000

    created main interface page & merged api files

--- data_gathering/data_api.py
+++ data_gathering/data_api.py
@@ -5,3 +5,12 @@ def geocode(address):
     places = requests.get(f"https://nominatim.openstreetmap.org/search", params=params).json()
 
     return {'lat':float(places[0]['lat']),'lon':float(places[0]['lon'])}
+
+def bikepoint_api(app_key):
+
+    base_url = 'https://api.tfl.gov.uk/BikePoint'
+    params_dict = {'app_key':app_key}
+
+    res = requests.get(base_url,params_dict).json()
+
+    return res
