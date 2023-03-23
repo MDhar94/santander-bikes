cfa953cbec3edc831c22e01b7a029724b30d9984
Mischa Dhar <mischadhar94@gmail.com>
2023-03-23 17:18:55 +0000

    reformatted front end & changed map back to plotly express

--- visualizing/nearby_bikes.py
+++ visualizing/nearby_bikes.py
@@ -13,29 +13,46 @@ def bikepoints_near_me(query, dataframe):
 
     centre_coords = get_centre(query)
 
-    fig = go.Figure(go.Scattermapbox(mode = "markers"
-                                     , lon = dataframe['lon']
-                                     , lat = dataframe['lat']
-                                     , marker=go.scattermapbox.Marker(size=dataframe['number_bikes']
-                                                                      , color=dataframe['bike_availability']
-                                                                      , colorscale=['red','grey','green'])
-                                     , showlegend=False
-                                     , hovertext=dataframe['Name']
-                                     , hoverinfo="text"
-                                     )
-                    )
+    fig = go.Figure()
 
     fig.add_trace(go.Scattermapbox(mode = "markers"
                                    ,lon = [centre_coords['lon']], lat = [centre_coords['lat']]
                                    , marker = {'size': 20, 'symbol': ["marker"]}
                                    , text = ["You are here!"]
-                                   , textposition = "bottom right"
-                                   , name='You are here!',showlegend=False
-                                   , hoverinfo="text"))
+                                   , name='You are here!'
+                                   ,showlegend=False))
 
-    fig.update_layout(mapbox=dict(accesstoken=token
+    fig.add_traces(
+        list(px.scatter_mapbox(dataframe,
+                            lat='lat',
+                            lon='lon',
+                            color='bike_availability',
+                            size='number_bikes',
+                            zoom=15,
+                            center=centre_coords,
+                            color_continuous_scale=['red', 'grey', 'green'],
+                            mapbox_style='carto-positron',
+                            width=800,
+                            height=600,
+                            labels={
+                                'number_bikes': 'Number of bikes',
+                                'bike_availability': '% of total',
+                                'Name': 'Bikepoint name'
+                            },
+                            hover_name='Name',
+                            hover_data={
+                                'Name': False,
+                                'number_bikes': True,
+                                'bike_availability': True,
+                                'lat': False,
+                                'lon': False
+                            }).select_traces()))
+
+    fig.update_layout(width=800
+                      , height=600
+                      , mapbox=dict(accesstoken=token
                                     , center=go.layout.mapbox.Center(lat=centre_coords['lat']
                                                                      , lon=centre_coords['lon'])
-                                    , zoom=14))
+                                    , zoom=15))
 
     return fig
